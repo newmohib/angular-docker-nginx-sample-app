b820591170829c93b703bceebfcbbac469141104
newmohib <@@mohib100@@>
2024-10-19 19:34:38 +0600

    added routering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ // Export the routes array as "routes"
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+];
